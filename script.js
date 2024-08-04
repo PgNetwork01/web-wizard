92345a86c7f24dcf3a1ae865594a287edf743ecd
Pg Network <gamerzp780@gmail.com>
2024-08-04 15:29:20 +0530

    q

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggle = document.getElementById('navbar-toggle');
+    const navbarLinks = document.getElementById('navbar-links');
+
+    navbarToggle.addEventListener('click', function() {
+        navbarLinks.classList.toggle('active');
+    });
+});
